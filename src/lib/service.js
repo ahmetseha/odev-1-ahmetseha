bb4360dd60c41530635d2ff58412d35396ff6538
Ahmet <ahmetsehacar@gmail.com>
2020-12-23 18:16:49 +0300

    const

--- src/lib/service.js
+++ src/lib/service.js
@@ -4,8 +4,8 @@ const ENDPOINT = "https://jsonplaceholder.typicode.com";
 
 export const getData = async (userId) => {
   try {
-    let { data: user } = await axios.get(`${ENDPOINT}/users/${userId}`);
-    let { data: posts } = await axios.get(`${ENDPOINT}/posts?userId=${userId}`);
+    const { data: user } = await axios.get(`${ENDPOINT}/users/${userId}`);
+    const { data: posts } = await axios.get(`${ENDPOINT}/posts?userId=${userId}`);
     const data = {...user,posts};
     return data;
   } 
